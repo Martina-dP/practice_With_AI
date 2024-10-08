f4bca9e2ea66672d76d9a811d1302aa9a16b2f11
Martina del Pino <martudelpino@gmail.com>
2024-10-08 13:38:55 +0200

    local storage implementation and improvements

--- back/app.js
+++ back/app.js
@@ -8,7 +8,17 @@ const port = 3001;
 
 app.use(cors()); 
 
-const quotes = []; 
+const quotes = [ {id: 1, text:"soy texto1", },
+    {id: 2, text:"soy texto2", },
+    {id: 3, text:"soy texto3", },
+    {id: 4, text:"soy texto4", },
+    {id: 5, text:"soy texto5", },
+    {id: 6, text:"soy texto6", },
+    {id: 7, text:"soy texto7", },
+    {id: 8, text:"soy texto8", },
+    {id: 9, text:"soy texto9", },
+    {id: 10, text:"soy texto10", }
+]; 
 
 const openai = new OpenAI({
     apiKey: process.env.API_KEY,
@@ -24,34 +34,29 @@ async function generateUniqueQuote() {
         });
 
         const quote = response.choices[0].message.content.trim();
-        quotes.push(quote)
-        console.log("generated quote:", quote)
-        return quote
+
+        if (!quotes.includes(quote)) {2|
+            quotes.push(quote)
+            console.log("generated quote:", quote)
+            return quote
+        }
+
+        return await generateUniqueQuote()
         
     } catch (error) {
         console.error("Error calling OpenAI API:", error.response ? error.response.data : error.message);
     }
 }
 
-function getRandomQuote() {
-    if (quotes.length === 0) {
-        return  null
-    }
-    const randomIndex = Math.floor(Math.random() * quotes.length); 
-    return quotes[randomIndex]; 
-}
-
-
 app.get('/api/quote', async (req, res) => {
 
     try {
         if (quotes.length < 100) {
-            
-        await generateUniqueQuote()
+            const newQuote = await generateUniqueQuote() 
+            res.status(200).json(quotes)
+        } else {
+            res.status(400).json({msj: "no hay mas quotes"})
         }
-        const randomQuote = getRandomQuote(); 
-        console.log(randomQuote, "Random quote")
-        res.status(200).json(randomQuote)
 
     } catch (error) {
         res.status(500).json({error : "It is not possible to generate quote"})
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,27 +1,72 @@
 import './App.css';
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
 
   const [quote, setQuote] = useState(null);
+  const [allQuotes, setAllQuotes] = useState([]);
+  const [shownQuotes, setShownQuotes] = useState([]);
+  const [allQuotesDisplayed, setAllQuotesDisplayed] = useState(false)
 
   const fetchQuote = async () => {
     try {
       const response = await fetch('http://localhost:3001/api/quote');
       const data = await response.json();
-      setQuote(data); 
+      setAllQuotes(data); 
       console.log(data)
     } catch (error) {
       console.error('Error fetching the quote:', error);
     }
   };
 
+  const loadQuotes = () => {
+    const quotesSaved = localStorage.getItem('shownQuotes')
+    if (quotesSaved) {
+      setShownQuotes(JSON.parse(quotesSaved))
+    }
+  }
+
+  const saveShownQuotes = (text) => {
+    const updateShownQuotes = [...shownQuotes, text]
+    setShownQuotes(updateShownQuotes)
+    localStorage.setItem("shownQuotes", JSON.stringify(updateShownQuotes))
+  }
+
+  const randomQuote = async () => {
+    if (allQuotes.length === 0) {
+        return  null
+    }
+
+    const filterQuotesShown = allQuotes.filter(f => !shownQuotes.includes(f.text))
+
+    if (filterQuotesShown.length === 0 ) {
+      setAllQuotesDisplayed(true)
+    }
+
+    const randomQuote = Math.floor(Math.random() * filterQuotesShown.length); 
+    const newQuote = filterQuotesShown[randomQuote]
+
+    if (newQuote) {
+      setQuote(newQuote)
+      saveShownQuotes(newQuote.text)
+      setAllQuotesDisplayed(false)
+  }
+  }
+
+  useEffect(() => {
+    fetchQuote()
+    loadQuotes()
+  }, [])
+
   return (
     <div className="App" style={{ textAlign: 'center', marginTop: '50px' }}>
       <h1>Random Quote Generator</h1>
-      {quote ? <p>{quote.text}</p> : <p>Haz clic en el botón para mostrar una cita.</p>}
-      {quote ? <button onClick={fetchQuote}>Get Another Quote</button> : <button onClick={fetchQuote}>Get Quote</button>}
+      {allQuotesDisplayed ? 
+        <p>There are no more quotes</p> : (
+          quote ? <p>{quote.text}</p> : <p>Haz clic en el botón para mostrar una cita.</p>
+        )}
+      <button onClick={randomQuote}> {quote ? "Get another quote" : "Get quote"} </button>
     </div>
   );
 }
